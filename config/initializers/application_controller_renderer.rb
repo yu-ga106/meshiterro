823540634de12acff037f4451021035b70978384
yu-ga106 <1006yg@gmail.com>
2021-07-14 06:08:54 +0000

    終了

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'd8c64fc6ce8dffc7ceaf1b27d2c7c1f181e7177823c68197b134f7a871ee578d7fcdd90a22238533866e9a45a68f9734abee4432aca1c837af034c05d51b65d1'
